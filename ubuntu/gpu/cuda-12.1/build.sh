04036d2e46c14431e701c44aa362ac43d432b1b9
Kota Tsuyuzaki <bloodeagle40234@gmail.com>
2024-04-18 17:40:36 +0900

    Add TensorRT-LLM Dockerfile

--- ubuntu/gpu/cuda-12.1/build.sh
+++ ubuntu/gpu/cuda-12.1/build.sh
@@ -6,3 +6,4 @@ docker build -t databricksruntime/gpu-base:cuda12.1 base/
 docker build -t databricksruntime/gpu-venv:cuda12.1 venv/
 docker build -t databricksruntime/gpu-tensorflow:cuda12.1 tensorflow/
 docker build -t databricksruntime/gpu-pytorch:cuda12.1 pytorch/
+docker build -t databricksruntime/gpu-trtllm:cuda12.1 trtllm/
